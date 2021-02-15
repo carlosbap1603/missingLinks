08b1febae7851fa2e41a9fc4c8ae077a9a0cc36f
Carlos Badillo <carlosbap1603@gmail.coml>
2021-02-15 20:20:13 +0100

    Hits algorithm

--- src/main/scala/fr/lri/wikipedia/centrality/HITS.scala
+++ src/main/scala/fr/lri/wikipedia/centrality/HITS.scala
@@ -9,21 +9,25 @@ import org.apache.spark.sql.functions._
 
 private class HITS extends CentralityMeasure {
 
-  def getMapVector(originalGraph: Graph[WikiPage, Link], neighborhood: Set[Long])( implicit spark: SparkSession): Map[String, Double] = {
-
-    val map = if (!neighborhood.isEmpty) {
-
-      val neighborhoodGraph = originalGraph.subgraph(vpred = (id, _) => neighborhood.contains(id))
-
-      val hitsGraph = Hits.initialize(neighborhoodGraph,10)
-
-      hitsGraph.vertices.map { case (id, vertexAttr) =>
-        Map(id.toString -> vertexAttr.authScore )
-      }.reduce((a, b) => a ++ b)
-
-    } else {
-      Map[String, Double]()
-    }
-
-    map
+//  def getMapVector(originalGraph: Graph[WikiPage, Link], neighborhood: Set[Long])( implicit spark: SparkSession): Map[String, Double] = {
+//
+//    val map = if (!neighborhood.isEmpty) {
+//
+//      val neighborhoodGraph = originalGraph.subgraph(vpred = (id, _) => neighborhood.contains(id))
+//
+//      val hitsGraph = Hits.initialize(neighborhoodGraph,10)
+//
+//      hitsGraph.vertices.map { case (id, vertexAttr) =>
+//        Map(id.toString -> vertexAttr.authScore )
+//      }.reduce((a, b) => a ++ b)
+//
+//    } else {
+//      Map[String, Double]()
+//    }
+//
+//    map
+//  }
+  override def getMapVector(originalGraph: Graph[WikiPage, Link], neighborhood: Set[Long]): Map[String, Double] = {
+  Map[String, Double]()
   }
+}
